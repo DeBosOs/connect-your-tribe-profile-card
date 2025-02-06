44953d6e001a8f1786c3220210e7518681a6c1ed
DeBosOs <luc.hoogerwerf@hva.nl>
2025-02-06 14:45:03 +0100

    styling, animatie, tekst en comments toegevoegd

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
+// Voer de functie pas uit als alle content is geladen
 document.addEventListener("DOMContentLoaded", function () {
+    // Selecteer alle buttons als "expandable"
     const buttons = document.querySelectorAll(".expandable");
 
+    // Loop door alle gevonden knoppen
     buttons.forEach(function (button) {
+        // Voeg een klik event listener toe aan elke knop
         button.addEventListener("click", function () {
+            // Toggle de class "expanded" als er op de knop geklikt wordt
             button.classList.toggle("expanded");
         });
     });
